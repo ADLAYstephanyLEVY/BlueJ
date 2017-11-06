3dc9551b8590be9bf8e744cf14db16942bc934bd
ADLAYstephanyLEVY <32655055+ADLAYstephanyLEVY@users.noreply.github.com>
2017-11-05 19:06:50 -0600

    Add files via upload

--- LZW.java
+++ LZW.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class LZW
 {
     public static void main (String [] args){
-       Hashtable<String, Integer> diccionario = new Hashtable<String, Integer>();
+        Hashtable<String, Integer> diccionario = new Hashtable<String, Integer>();
         int num=0;
         String t = JOptionPane.showInputDialog("Ingresa letras: ");
-        
+
         /* capturar texto
          * crear diccionario inicial
          *  ciclo para letras del 97 a 122
@@ -33,12 +33,27 @@ public class LZW
          *              num le asigno -1
          *              limpiar prefijo
          */
-      /*  for(int j=0; j<texto.length; j++){
-            char letra = texto.indexOf(j);
-            if(letra)
-        } */
+        int cap = -1;
+        ArrayList<Integer>comp = new ArrayList<Integer>();
+        String tud = "";
+        for(int j=0; j<t.length(); j++){
+            tud += t.charAt(j);
+            if(diccionario.containsKey(tud)){
+                cap = diccionario.get(tud);
+                //j++;
+            }else {
+                diccionario.put(tud, cap++);
+                comp.add(cap);
+                cap=-1;
+                tud="";
+            }
+        }
+        if(cap != -1){
+            comp.add(cap);
+        }
         //imptimir diccionario y comprimido
         System.out.println(diccionario);
+        System.out.println(comp);
     }
 
 }
